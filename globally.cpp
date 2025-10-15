b295ba2f02746bf9b71fce0961cc5c3e7681e849
ushaboya11 <24B11DS032@adityauniversity.in>
2025-10-15 14:09:08 +0530

    Add files via upload

---
+++ globally.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+ 
+int x = 10; // global variable 
+
+int main() { 
+int x = 20; // local variable 
+
+std::cout << "Local x: " << x << std::endl;
+ 
+std::cout << "Global x: " << ::x << std::endl;  
+                     // scope resolution to access global x 
+return 0;
+}
+
